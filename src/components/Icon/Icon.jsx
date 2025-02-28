4257287a1d8cd183145e24c26c14fbf65068c616
viktoria naskova <smorodinka-gg@mail.ru>
2025-02-28 19:35:10 +0300

    button & icon is ready

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames'
+import './Icon.scss'
+import { Icon as MinistaIcon } from 'minista'
+
+const Icon = (props) => {
+    const {
+    className,
+    name,
+    hasFill = false,
+} = props
+
+return (
+    <span className={classNames(className, 'icon')}>
+        <MinistaIcon 
+            iconId={name}
+            fill={hasFill ? 'currentColor' : 'none'}
+            stroke={hasFill ? 'nobe' : 'currentColor'}
+        />
+    </span>
+)
+}
+
+export default Icon 
