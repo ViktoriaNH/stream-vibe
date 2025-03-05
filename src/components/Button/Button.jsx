3400380b3b1462a8582c59a2fb09778d16c4f4f4
viktoria naskova <smorodinka-gg@mail.ru>
2025-03-05 22:55:32 +0300

    footer is ready

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,50 +1,46 @@
-import Icon from "../Icon"
-import "./Button.scss"
-import classNames from "classnames"
+import Icon from "../Icon";
+import "./Button.scss";
+import classNames from "classnames";
 
 const Button = (props) => {
-  const { 
+  const {
     className,
-    type = 'button',
+    type = "button",
     href,
     target,
-    mode = '',
+    mode = "",
     label,
     isLabelHidden = false,
     iconName,
-// 'before' | 'after'
-    iconPosition = 'before',
- } = props
+    // 'before' | 'after'
+    iconPosition = "before",
+    hasFillIcon,
+  } = props;
 
-  const isLink = href !== undefined
-  const Component = isLink ? "a" : "button"
-  const linkProps = { href, target }
-  const buttonProps = { type }
-  const specificProps = isLink ? linkProps : buttonProps
-  const title = isLabelHidden ? label : undefined
+  const isLink = href !== undefined;
+  const Component = isLink ? "a" : "button";
+  const linkProps = { href, target };
+  const buttonProps = { type };
+  const specificProps = isLink ? linkProps : buttonProps;
+  const title = isLabelHidden ? label : undefined;
   const iconComponent = iconName && (
-    <Icon 
-        className="button__icon"
-        name={iconName}
-    />
-)
+    <Icon className="button__icon" name={iconName} hasFill={hasFillIcon} />
+  );
 
   return (
     <Component
-    className={classNames(className, 'button', {
+      className={classNames(className, "button", {
         [`button--${mode}`]: mode,
-    })}
-    title={title}
-    aria-label={title}
-    {...specificProps}
+      })}
+      title={title}
+      aria-label={title}
+      {...specificProps}
     >
-        {iconPosition === 'before' && iconComponent}
-        {!isLabelHidden && (
-            <span className="button__label">{label}</span>
-        )}
-        {iconPosition === 'after' && iconComponent}
+      {iconPosition === "before" && iconComponent}
+      {!isLabelHidden && <span className="button__label">{label}</span>}
+      {iconPosition === "after" && iconComponent}
     </Component>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
