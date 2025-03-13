e4f02ddd2eb7c19dfc88e558e4f3a95e701ee83b
viktoria naskova <smorodinka-gg@mail.ru>
2025-03-13 17:14:44 +0300

    Hero, Section are ready

---
+++ src/sections/Categories/Categories.jsx
@@ -0,0 +1,22 @@
+import Section from "@/layouts/Section";
+
+const Categories = () => {
+  return (
+    <Section
+      title="Explore our wide variety of categories"
+      titleId="categories-title"
+      description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
+      actions={
+        <div>
+          <button>hgfd</button>
+          <button>kjhgfd</button>
+        </div>
+      }
+      isActionsHiddenOnMobile
+    >
+
+    </Section>
+  );
+};
+
+export default Categories;
